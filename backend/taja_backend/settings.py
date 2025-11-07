02b186a78c8a09c52b252a9993454a060f067faa
Eden Iyanda <106480499+edeniyanda@users.noreply.github.com>
2025-11-07 20:17:57 +0100

    Final commit

--- backend/taja_backend/settings.py
+++ backend/taja_backend/settings.py
@@ -91,6 +91,18 @@ DATABASES = {
 }
 
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",  # Redis DB index 1
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+        "KEY_PREFIX": "geocode"
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
